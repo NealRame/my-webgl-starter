a96d8207b742845356c9f3f9bf7a9b27313f11ae
Julien Graziano <julien.graziano@arobas-music.com>
2023-11-19 16:44:03 +0100

    fix: add missing files

---
+++ src/applets/8/shaders/vertex.glsl
@@ -0,0 +1,13 @@
+#version 300 es
+
+in vec4 a_position;
+in vec3 a_normal;
+
+uniform mat4 u_transform;
+
+out vec3 v_normal;
+
+void main() {
+    gl_Position = u_transform*a_position;
+    v_normal = a_normal;
+}
